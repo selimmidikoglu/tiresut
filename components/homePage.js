32c6beb60f764b052bbb8754b426f061496186cd
selimmidikoglu <selim.midikoglu@ug.bilkent.edu.tr>
2019-01-19 16:10:38 +0300

    Google maps current location zoom

--- components/homePage.js
+++ components/homePage.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Platform, StyleSheet, Text, View,ActivityIndicator} from 'react-native';
 import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
 import Geocoder from 'react-native-geocoding';
 import { createStackNavigator, createAppContainer } from "react-navigation";
@@ -14,16 +14,21 @@ export default class HomePage extends Component{
     super(props);
 
     this.state = {
-      latitude: null,
-      longitude: null,
+      latitude: 38.41885,
+      longitude: 27.12872,
       error:null,
+      loading: true,
     };
   }
   componentDidMount(){
+
     navigator.geolocation.getCurrentPosition(
       (position) => {
-        console.log("wokeeey");
         console.log(position);
+        var lat = toString(position.coords.latitude);
+        console.log(lat);
+        var lon = toString(position.coords.longitude);
+        console.log(lon);
         this.setState({
           latitude: position.coords.latitude,
           longitude: position.coords.longitude,
@@ -31,40 +36,64 @@ export default class HomePage extends Component{
         });
       },
       (error) => this.setState({ error: error.message }),
-      { enableHighAccuracy: false, timeout: 200000, maximumAge: 1000 },
+      { enableHighAccuracy: true, timeout: 200000, maximumAge: 1000 },
     );
     Geocoder.init('AIzaSyCyG67U79Ez16ov4XcDofAociXdDxIsOL8'); // use a valid API key
-    Geocoder.from("Colosseum")
+    Geocoder.from("Erzene Mahallesi")
         .then(json => {
-            var location = json.results//[0].geometry.location;
-            //console.log(location);
+            var location = json.results[0].geometry.location;
+            console.log("Lokasyon adından" + json.results);
         })
         .catch(error => console.warn(error));
  
-    Geocoder.from(38.4653,27.2267)
+    Geocoder.from(38.46549058,27.22578868)
         .then(json => {
         	var addressComponent = json.results//s[0].address_components[0];
           console.log(addressComponent);
         })
         .catch(error => console.warn(error));
+       setInterval(this.setState({loading:false}),5000);
   }
-  
-  render() {
-    return (
-      <View style={styles.container}>
-      <MapView
-        provider={PROVIDER_GOOGLE} // remove if not using Google Maps
-        style={styles.map}
-        region={{
-          latitude: 38.4237,
-          longitude: 27.1428,
-          latitudeDelta: 0.015,
-          longitudeDelta: 0.0121,
+ 
+  renderMapView(){
+    if(this.state.loading)
+    {
+      return <ActivityIndicator size="large" color="yellow" />;
+    }else{
+      return (
+        <View style={styles.container}>
+        <MapView
+          provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+          style={styles.map}
+          initialRegion={{
+            latitude: this.state.latitude,
+            longitude: this.state.longitude,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
         }}
-      >
-      </MapView>
-    </View>
-    );
+        showUserLocation
+        >
+         <MapView.Marker
+              showUserLocation
+              coordinate={{latitude: this.state.latitude,
+              longitude: this.state.longitude}}
+              title={"Konumunuz"}
+              description={"Bulunduğunuz konum!"}
+           />
+        </MapView>
+      </View>
+      );
+    }
+  }
+ 
+  render() {
+    return(
+      <View style= {{flex:1,alignItems:'center',justifyContent:'center',height: '100%',width:'100%',backgroundColor: 'black'}}>
+        {this.renderMapView()}
+      </View>
+      
+      )
+    
   }
 }
 
@@ -76,7 +105,14 @@ const styles = StyleSheet.create({
     justifyContent: 'flex-end',
     alignItems: 'center',
   },
-  map: {
+  map1: {
     ...StyleSheet.absoluteFillObject,
   },
+  map: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+  }
  }); 
