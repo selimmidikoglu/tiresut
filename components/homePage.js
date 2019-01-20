efadef8c7cf11799b90385cbfbcda61c9c66815f
selimmidikoglu <selim.midikoglu@ug.bilkent.edu.tr>
2019-01-21 01:42:59 +0300

    Temporary Tab Bar is added. Still have problems with rendering products with wrap  option.

--- components/homePage.js
+++ components/homePage.js
@@ -1,12 +1,15 @@
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View,ActivityIndicator} from 'react-native';
+import {Platform, Image,StyleSheet, Dimensions, Text, ScrollView, View,ActivityIndicator} from 'react-native';
 import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
 import Geocoder from 'react-native-geocoding';
 import { createStackNavigator, createAppContainer } from "react-navigation";
 import { TabNavigator } from "react-navigation";
 import  GlobalStore  from './globalStore';
-
-
+import Icon from 'react-native-vector-icons/Ionicons';
+var dimensions ={
+  height: Dimensions.get('window').height,
+  width: Dimensions.get('window').width
+}
 
 export default class HomePage extends Component{
   static navigationOptions = {
@@ -16,6 +19,7 @@ export default class HomePage extends Component{
     super(props);
 
     this.state = {
+      products : [],
       latitude: GlobalStore.latitude,
       longitude: GlobalStore.longitute,
       error:null,
@@ -23,23 +27,16 @@ export default class HomePage extends Component{
     };
   }
   componentDidMount(){
-
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        console.log(position);
-        var lat = toString(position.coords.latitude);
-        console.log(lat);
-        var lon = toString(position.coords.longitude);
-        console.log(lon);
-        this.setState({
-          latitude: position.coords.latitude,
-          longitude: position.coords.longitude,
-          error: null,
-        });
-      },
-      (error) => this.setState({ error: error.message }),
-      { enableHighAccuracy: true, timeout: 200000, maximumAge: 1000 },
-    );
+    fetch('http://192.168.1.45:3000/products')
+    .then((response) => response.json())
+    .then((responseJson) => {
+      this.setState({products:responseJson})
+      console.log(this.state.products);
+      this.setState({loading:false})
+    })
+    .catch((error) => {
+      console.error(error);
+    });
     Geocoder.init('AIzaSyCyG67U79Ez16ov4XcDofAociXdDxIsOL8'); // use a valid API key
     Geocoder.from("Erzene Mahallesi")
         .then(json => {
@@ -50,11 +47,11 @@ export default class HomePage extends Component{
  
     Geocoder.from(38.46549058,27.22578868)
         .then(json => {
-        	var addressComponent = json.results//s[0].address_components[0];
+        	var addressComponent = json//s[0].address_components[0];
           console.log(addressComponent);
         })
         .catch(error => console.warn(error));
-       setInterval(this.setState({loading:false}),5000);
+       
   }
  
   renderMapView(){
@@ -66,12 +63,12 @@ export default class HomePage extends Component{
         <View style={styles.container}>
         <MapView
           provider={PROVIDER_GOOGLE} // remove if not using Google Maps
-          style={styles.map}
+          style={styles.map1}
           initialRegion={{
             latitude: this.state.latitude,
             longitude: this.state.longitude,
-            latitudeDelta: 1,
-            longitudeDelta: 1
+            latitudeDelta: 0.09,
+            longitudeDelta: 0.09
         }}
         showUserLocation
         >
@@ -84,22 +81,60 @@ export default class HomePage extends Component{
       );
     }
   }
+  renderProductsView(){
+    let productsView = [];
+      productsView = this.state.products.map((product) => (
+      <View key={product.name} style={{height:dimensions.width/2, width:'50%'}}>
+          <View style={{flex:1,borderRadius:25,alignItems:'center',justifyContent:'center',overflow: 'hidden'}}>
+            <Image style= {{height: dimensions.width/2-10,width:dimensions.width/2-10}} resizeMode="cover" source = {{uri:product.imageUrl}}></Image>
+          </View>
+        </View>
+        ))
+      return productsView;
+  }
  
+
   render() {
     return(
-      <View style= {{flex:1,alignItems:'center',justifyContent:'center',height: '100%',width:'100%',backgroundColor: 'black'}}>
-        {this.renderMapView()}
+      <View style= {{flex:1,height: '100%',width:'100%',backgroundColor: 'rgba(0,0,0,0)'}}>
+        <View style={{flex:1,flexDirection:'row',backgroundColor:'green'}}>
+          <View style={{flex:3}}/>{/*Top Bar Left Empty*/} 
+          <View style={{flex:1,alignItems:'center',justifyContent:'center'}}>
+            <Image  resizeMode="cover" style={{height:50,width:200}} source={require('../assets/images/inekbuyuk.png')}></Image>
+          </View>
+          <View style={{flex:3,justifyContent:'center',alignItems:'center',flexDirection:'row', marginRight:10}}>{/*Top Bar Right With Sepet*/}
+          <View style={{flex:5}}></View>{/*Empty Right Space*/}
+          <View style={{flex:1,marginLeft:10,height:15,width:15,borderBottomColor:'white',borderBottomWidth:0.9, borderRadius:10, alignItems:'center',justifyContent:'center'}}><Text style={{fontSize:9,color:'white'}}>+1</Text></View>
+          <View style={{flex:2}}><Icon name="md-basket" size={30} color="white" /></View>
+          
+          </View>
+        </View>
+        <View style= {{flex:5}}>{/*Map View*/}
+          {this.renderMapView()}
+        </View>
+        <View style={{flex:5,backgroundColor:'red',alignItems:'flex-start'}}>
+        <ScrollView contentContainerStyle={{borderWidth:2,borderColor:'green',flexWrap:'wrap'}}>
+        
+          
+          {this.renderProductsView()}
+       
+       
+            
+          
+         
+        
+        </ScrollView> 
+        </View>
+        <View style={{flex:1,flexDirection:'row',backgroundColor:'green'}}></View>
       </View>
-      
-      )
-    
+    )  
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     ...StyleSheet.absoluteFillObject,
-    height: 400,
+    height: 300,
     width: 400,
     justifyContent: 'flex-end',
     alignItems: 'center',
@@ -108,7 +143,7 @@ const styles = StyleSheet.create({
     ...StyleSheet.absoluteFillObject,
   },
   map: {
-    position: 'absolute',
+    position:'absolute',
     top: 0,
     left: 0,
     right: 0,
